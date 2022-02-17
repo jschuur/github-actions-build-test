a583b06e0422e57a855233a9c493052ae5be811e
Joost Schuur <jschuur@jschuur.com>
2022-02-17 12:24:44 +0000

    Use Action payload

--- src/hello.js
+++ src/hello.js
@@ -1,5 +1,11 @@
 import 'dotenv/config';
 
-if (process.env.NAME === 'Bobby') console.log('Good Day, Sir Bobby!');
-else console.log(`Hello ${process.env.NAME || 'stranger'}!`);
+const name = process.env.NAME;
 
+if (name) {
+  console.log(`name is type ${typeof name}`);
+  console.log(name);
+}
+
+if (name === 'Bobby') console.log('Good Day, Sir Bobby!');
+else console.log(`Hello ${name || 'stranger'}!`);
